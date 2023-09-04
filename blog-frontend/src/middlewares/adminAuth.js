fb0848abb50578bb8b0698dde9c1995c40371340
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-09-04 19:37:10 -0300

    fix: add session creation in frontend

--- blog-frontend/src/middlewares/adminAuth.js
+++ blog-frontend/src/middlewares/adminAuth.js
@@ -1,9 +1,9 @@
 function adminAuth(req, res, next) {
-  // if (req.session.user != undefined) {
-  next();
-  // } else {
-  //   res.redirect('/login');
-  // }
+  if (req.session.user != undefined) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
 }
 
 module.exports = adminAuth;
